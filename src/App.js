79b6ba91ed704dbb9e911cd4b42b0bf849cdab01
Josué Araújo <josue.araujo@esig.com.br>
2020-05-11 23:57:31 -0300

    All repositories are being listed in the simple interface and all the test cases have been successful.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+   api.get('repositories').then(response => {
+     setRepositories(response.data);
+   });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const newRepo = {
+      url: "https://github.com/josuearaujo/new-repo",
+      title: `Repository ${Date.now()}`,
+      techs: ["React", "Node.js"],
+    };
+
+    const addedRepo = await api.post('/repositories', newRepo);
+
+    setRepositories([...repositories, addedRepo.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`)
+      .then(response => {
+        const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+        
+        repositories.splice(repositoryIndex, 1);
+        setRepositories([...repositories]);        
+      })
+      .catch(response => {
+        alert(response);
+      });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}          
+          
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+        {/* <li>
           Repositório 1
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
-        </li>
+        </li> */}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
